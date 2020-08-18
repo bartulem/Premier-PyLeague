b5515d37d76e50d0ac7f6810a40cd91dea3c6934
bartulem <mimica.bartul@gmail.com>
2020-08-18 12:34:33 +0200

    update players for 2019/20

--- scrape_PL_data.py
+++ scrape_PL_data.py
@@ -7,19 +7,21 @@ import pandas as pd
 from tqdm import tqdm
 import pickle
 
-seasonsID = {'2007-08': '16', '2008-09': '17', '2009-10': '18', '2010-11': '19', '2011-12': '20', '2012-13': '21', '2013-14': '22', '2014-15': '27', '2015-16': '42', '2016-17': '54', '2017-18': '79', '2018-19': '210'}
+seasonsID = {'2007-08': '16', '2008-09': '17', '2009-10': '18', '2010-11': '19', '2011-12': '20', '2012-13': '21', '2013-14': '22', '2014-15': '27', '2015-16': '42',
+             '2016-17': '54', '2017-18': '79', '2018-19': '210', '2019-20': '274'}
 playerID = {'David de Gea': '4330', 'Ashley Young': '3062', 'Antonio Valenica': '3285', 'Chris Smalling': '3613', 'Phil Jones': '3820',
             'Jonny Evans': '3156', 'Daley Blind': '4888', 'Marcos Rojo': '10472', 'Luke Shaw': '4608', 'Eric Bailly': '12937',
             'Matteo Darmian': '12854', 'Victor Lindelöf': '5066', 'Diogo Dalot': '24252', 'Rafael da Silva': '3664', 'Rio Ferdinand': '1111',
             'Nemanja Vidic': '2935', 'Patrice Evra': '2934', 'Alexander Büttner': '4537', 'Fabio': '3665', 'Edwin van der Sar': '2070',
-            'Gary Neville': '321', 'Wes Brown': '1211', 'John OShea': '1717', 'Gerard Piqué': '2737',
+            'Gary Neville': '321', 'Wes Brown': '1211', 'John OShea': '1717', 'Gerard Piqué': '2737', 'Aaron Wan-Bissaka': '14164',
+            'Brandon Williams': '23834', 'Harry Maguire': '9566',
             'Joe Hart': '3143', 'Micah Richards': '2925', 'Pablo Zabaleta': '3654', 'Aleksandar Kolarov': '4141', 'Gaël Clichy': '2413',
             'Vincent Kompany': '3653', 'Joleon Lescott': '2574', 'Martín Demichelis': '4802', 'Matija Nastasic': '4532', 'Bacary Sagna': '3311',
             'Eliaquim Mangala': '5334', 'Claudio Bravo': '5810', 'John Stones': '4505', 'Nicolás Otamendi': '6058', 'Ederson': '12707',
             'Kyle Walker': '3955', 'Danilo': '5328', 'Benjamin Mendy': '5575', 'Aymeric Laporte': '11044', 'Maicon': '4531',
             'Stefan Savic': '4321', 'Wayne Bridge': '1458', 'Jérôme Boateng': '4142', 'Shay Given': '851', 'Javier Garrido': '3427',
             'Sylvinho': '1651', 'Nedum Onuoha': '2728', 'Richard Dunne': '1184', 'Michael Ball': '1185', 'Tal Ben Haim': '2645',
-            'Jihai Sun': '2296',
+            'Jihai Sun': '2296', 'João Cancelo': '5051', 'Angeliño': '10467',
             'Simon Mignolet': '4179', 'Glen Johnson': '2148', 'José Enrique': '3462', 'Aly Cissokho': '4797', 'Jon Flanagan': '4129',
             'Kolo Touré': '2002', 'Daniel Agger': '2916', 'Mamadou Sakho': '4796', 'Martin Skrtel': '3404', 'Javier Manquillo': '4918',
             'Alberto Moreno': '7219', 'Dejan Lovren': '4813', 'James Milner': '2100', 'Loris Karius': '19600', 'Nathaniel Clyne': '4604',
@@ -33,31 +35,33 @@ playerID = {'David de Gea': '4330', 'Ashley Young': '3062', 'Antonio Valenica': 
             'Kurt Zouma': '5175', 'Marcos Alonso': '4093', 'Willy Caballero': '10466', 'Davide Zappacosta': '25313', 'Antonio Rüdiger': '16801',
             'Andreas Christensen': '4495', 'Kepa': '11373', 'Emerson': '16803', 'Ryan Bertrand': '2886', 'José Bosingwa': '3587',
             'Paulo Ferreira': '2654', 'Yuri Zhirkov': '3860', 'Alex': '2655', 'Juliano Belletti': '3361', 'Ricardo Carvalho': '2653',
+            'Reece James': '14571', 'Kurt Zouma': '5175', 'Fikayo Tomori': '13281',
             'Wojciech Szczesny': '3543', 'Carl Jenkinson': '4248', 'Nacho Monreal': '4472', 'Kieran Gibbs': '3046', 'Per Mertesacker': '4246',
             'Thomas Vermaelen': '3796', 'Laurent Koscielny': '4030', 'David Ospina': '10420', 'Mathieu Debuchy': '4546', 'Héctor Bellerín': '4474',
             'Gabriel Paulista': '10423', 'Calum Chambers': '4620', 'Rob Holding': '11575', 'Shkodran Mustafi': '3869', 'Sead Kolasinac': '5368',
             'Konstantinos Mavropanos': '32638', 'Bernd Leno': '4985', 'Stephan Lichtsteiner': '5520', 'Sokratis': '5093', 'Johan Djourou': '2609',
             'Emmanuel Eboué': '2612', 'Sébastien Squillaci': '4031', 'Manuel Almunia': '2607', 'Armand Traoré': '2835', 'Sol Campbell': '557',
-            'Mikaël Silvestre': '1719', 'Philippe Senderos': '2412',
+            'Mikaël Silvestre': '1719', 'Philippe Senderos': '2412', 'Kieran Tierney': '12185', 'Calum Chambers': '4620',
             'Heurelho Gomes': '3719', 'Danny Rose': '3507', 'Younès Kaboul': '3508', 'Jan Vertonghen': '4666', 'Vlad Chiriches': '4840',
             'Hugo Lloris': '4664', 'Ben Davies': '4408', 'Federico Fazio': '7215', 'Kieran Trippier': '3905', 'Toby Alderweireld': '4916',
             'Eric Dier': '4112', 'Kevin Wimmer': '13813', 'Serge Aurier': '11293', 'Davinson Sánchez': '20224', 'Juan Foyth': '25309',
             'Benoît Assou-Ekotto': '3239', 'William Gallas': '2054', 'Michael Dawson': '2805', 'Ledley King': '1627', 'Alan Hutton': '3506',
             'Sébastien Bassong': '3681',  'Vedran Corluka': '3426', 'Jonathan Woodgate': '1565', 'Gareth Bale': '3513', 'Paul Robinson': '1404',
-            'Pascal Chimbonda': '3026', 'Yeong-pyo Lee': '2988'}
+            'Pascal Chimbonda': '3026', 'Yeong-pyo Lee': '2988', 'Paulo Gazzaniga': '4600'}
 
 injuryCodes = {'David de Gea': '59377', 'Ashley Young': '14086', 'Antonio Valenica': '33544', 'Chris Smalling': '103427', 'Phil Jones': '117996',
                'Jonny Evans': '42412', 'Daley Blind': '12282', 'Marcos Rojo': '93176', 'Luke Shaw': '183288', 'Eric Bailly': '286384',
                'Matteo Darmian': '54906', 'Victor Lindelöf': '184573', 'Diogo Dalot': '357147', 'Rafael da Silva': '61892', 'Rio Ferdinand': '3235',
                'Nemanja Vidic': '19726', 'Patrice Evra': '5285', 'Alexander Büttner': '38003', 'Fabio': '61891', 'Edwin van der Sar': '3516',
-               'Gary Neville': '3403', 'Wes Brown': '3405', 'John OShea': '3540', 'Gerard Piqué': '18944',
+               'Gary Neville': '3403', 'Wes Brown': '3405', 'John OShea': '3540', 'Gerard Piqué': '18944', 'Aaron Wan-Bissaka':  '477758',
+               'Brandon Williams':  '507700', 'Harry Maguire':  '177907',
                'Joe Hart': '40204', 'Micah Richards': '32617', 'Pablo Zabaleta': '20007', 'Aleksandar Kolarov': '46156', 'Gaël Clichy': '7449',
                'Vincent Kompany': '9594', 'Joleon Lescott': '4241', 'Martín Demichelis': '2963', 'Matija Nastasic': '143559', 'Bacary Sagna': '26764',
                'Eliaquim Mangala': '90681', 'Claudio Bravo': '40423', 'John Stones': '186590', 'Nicolás Otamendi': '54781', 'Ederson': '238223',
                'Kyle Walker': '95424', 'Danilo': '145707', 'Benjamin Mendy': '157495', 'Aymeric Laporte': '176553', 'Maicon': '18301',
                'Stefan Savic': '107010', 'Wayne Bridge': '3682', 'Jérôme Boateng': '26485', 'Shay Given': '3146', 'Javier Garrido': '23218',
                'Sylvinho': '3359', 'Nedum Onuoha': '28810', 'Richard Dunne': '3807', 'Michael Ball': '9423', 'Tal Ben Haim': '20714',
-               'Jihai Sun': '3806',
+               'Jihai Sun': '3806', 'João Cancelo': '182712', 'Angeliño': '277179',
                'Simon Mignolet': '50219', 'Glen Johnson': '3881', 'José Enrique': '35571', 'Aly Cissokho': '57515', 'Jon Flanagan': '145922',
                'Kolo Touré': '3202', 'Daniel Agger': '22832', 'Mamadou Sakho': '47713', 'Martin Skrtel': '24180', 'Javier Manquillo': '162029',
                'Alberto Moreno': '207917', 'Dejan Lovren': '37838', 'James Milner': '3333', 'Loris Karius': '85864', 'Nathaniel Clyne': '85177',
@@ -71,18 +75,19 @@ injuryCodes = {'David de Gea': '59377', 'Ashley Young': '14086', 'Antonio Valeni
                'Kurt Zouma': '157509', 'Marcos Alonso': '112515', 'Willy Caballero': '19948', 'Davide Zappacosta': '173859', 'Antonio Rüdiger': '86202',
                'Andreas Christensen': '196948', 'Kepa': '192279', 'Emerson': '181778', 'Ryan Bertrand': '40611', 'José Bosingwa': '9813',
                'Paulo Ferreira': '9832', 'Yuri Zhirkov': '16760', 'Alex': '15420', 'Juliano Belletti': '7829', 'Ricardo Carvalho': '9828',
+               'Reece James': '472423', 'Kurt Zouma': '157509', 'Fikayo Tomori': '303254',
                'Wojciech Szczesny': '44058', 'Carl Jenkinson': '126321', 'Nacho Monreal': '43003', 'Kieran Gibbs': '44792', 'Per Mertesacker': '6710',
                'Thomas Vermaelen': '15904', 'Laurent Koscielny': '76277', 'David Ospina': '73396', 'Mathieu Debuchy': '27306', 'Héctor Bellerín': '191217',
                'Gabriel Paulista': '149498', 'Calum Chambers': '215118', 'Rob Holding': '253341', 'Shkodran Mustafi': '88590', 'Sead Kolasinac': '94005',
                'Konstantinos Mavropanos': '415912', 'Bernd Leno': '72476', 'Stephan Lichtsteiner': '2865', 'Sokratis': '34322', 'Johan Djourou': '34561',
                'Emmanuel Eboué': '13058', 'Sébastien Squillaci': '5293', 'Manuel Almunia': '16621', 'Armand Traoré': '33783', 'Sol Campbell': '3198',
-               'Mikaël Silvestre': '3393', 'Philippe Senderos': '4277',
+               'Mikaël Silvestre': '3393', 'Philippe Senderos': '4277', 'Kieran Tierney': '300716', 'Calum Chambers': '215118',
                'Heurelho Gomes': '19059', 'Danny Rose': '50174', 'Younès Kaboul': '27114', 'Jan Vertonghen': '43250', 'Vlad Chiriches': '123261',
                'Hugo Lloris': '17965', 'Ben Davies': '192765', 'Federico Fazio': '45314', 'Kieran Trippier': '95810', 'Toby Alderweireld': '42710',
                'Eric Dier': '175722', 'Kevin Wimmer': '122675', 'Serge Aurier': '127032', 'Davinson Sánchez': '341429', 'Juan Foyth': '480763',
                'Benoît Assou-Ekotto': '18310', 'William Gallas': '3156', 'Michael Dawson': '9988', 'Ledley King': '3360', 'Alan Hutton': '9619',
                'Sébastien Bassong': '33951', 'Vedran Corluka': '34393', 'Jonathan Woodgate': '3224', 'Gareth Bale': '39381', 'Paul Robinson': '3630',
-               'Pascal Chimbonda': '18875', 'Yeong-pyo Lee': '6156'}
+               'Pascal Chimbonda': '18875', 'Yeong-pyo Lee': '6156', 'Paulo Gazzaniga': '195488'}
 
 playersBYseasons = {'2007-08': {'MU': {'GK': ['Edwin van der Sar'], 'RB': ['Wes Brown', 'John OShea'], 'LB': ['Patrice Evra'], 'CB': ['Rio Ferdinand', 'Nemanja Vidic', 'Gerard Piqué', 'Mikaël Silvestre']},
                                 'MC': {'GK': ['Joe Hart'], 'RB': ['Micah Richards'], 'LB': ['Michael Ball', 'Javier Garrido'], 'CB': ['Nedum Onuoha', 'Jihai Sun', 'Richard Dunne', 'Vedran Corluka']},
@@ -155,7 +160,13 @@ playersBYseasons = {'2007-08': {'MU': {'GK': ['Edwin van der Sar'], 'RB': ['Wes 
                                 'LFC': {'GK': ['Alisson'], 'RB': ['Trent Alexander-Arnold'], 'LB': ['Andrew Robertson'], 'CB': ['Virgil van Dijk', 'Dejan Lovren', 'Joe Gomez', 'Joël Matip']},
                                 'CFC': {'GK': ['Kepa', 'Willy Caballero'], 'RB': ['César Azpilicueta'], 'LB': ['Marcos Alonso', 'Emerson'], 'CB': ['Antonio Rüdiger', 'Andreas Christensen', 'David Luiz']},
                                 'AFC': {'GK': ['Bernd Leno'], 'RB': ['Héctor Bellerín', 'Stephan Lichtsteiner'], 'LB': ['Nacho Monreal', 'Sead Kolasinac'], 'CB': ['Laurent Koscielny', 'Rob Holding', 'Shkodran Mustafi', 'Konstantinos Mavropanos', 'Sokratis']},
-                                'TH': {'GK': ['Hugo Lloris'], 'RB': ['Kieran Trippier', 'Serge Aurier'], 'LB': ['Danny Rose', 'Ben Davies'], 'CB': ['Toby Alderweireld', 'Jan Vertonghen', 'Davinson Sánchez', 'Juan Foyth']}}}
+                                'TH': {'GK': ['Hugo Lloris'], 'RB': ['Kieran Trippier', 'Serge Aurier'], 'LB': ['Danny Rose', 'Ben Davies'], 'CB': ['Toby Alderweireld', 'Jan Vertonghen', 'Davinson Sánchez', 'Juan Foyth']}},
+                    '2019-20': {'MU': {'GK': ['David de Gea'], 'RB': ['Ashley Young', 'Aaron Wan-Bissaka', 'Diogo Dalot'], 'LB': ['Luke Shaw', 'Brandon Williams'], 'CB': ['Harry Maguire', 'Phil Jones', 'Eric Bailly', 'Victor Lindelöf']},
+                                'MC': {'GK': ['Ederson'], 'RB': ['Kyle Walker', 'João Cancelo'], 'LB': ['Benjamin Mendy', 'Angeliño'], 'CB': ['John Stones', 'Nicolás Otamendi', 'Aymeric Laporte']},
+                                'LFC': {'GK': ['Alisson'], 'RB': ['Trent Alexander-Arnold'], 'LB': ['Andrew Robertson'], 'CB': ['Virgil van Dijk', 'Dejan Lovren', 'Joe Gomez', 'Joël Matip']},
+                                'CFC': {'GK': ['Kepa'], 'RB': ['César Azpilicueta', 'Reece James'], 'LB': ['Marcos Alonso', 'Emerson'], 'CB': ['Antonio Rüdiger', 'Andreas Christensen', 'Kurt Zouma', 'Fikayo Tomori']},
+                                'AFC': {'GK': ['Bernd Leno'], 'RB': ['Héctor Bellerín'], 'LB': ['Kieran Tierney', 'Sead Kolasinac'], 'CB': ['Calum Chambers', 'David Luiz', 'Rob Holding', 'Shkodran Mustafi', 'Sokratis']},
+                                'TH': {'GK': ['Hugo Lloris', 'Paulo Gazzaniga'], 'RB': ['Serge Aurier'], 'LB': ['Danny Rose', 'Ben Davies'], 'CB': ['Toby Alderweireld', 'Jan Vertonghen', 'Davinson Sánchez', 'Juan Foyth']}}}
 
 
 def getIndex(spicificseason):
@@ -185,7 +196,7 @@ def getColumns(spicificseason):
 
 
 def getInjuries(aplayer, theseason):
-    aplayerCleaned = aplayer.lower().replace(' ', '-').replace('ö', 'o').replace('ë', 'e').replace('á', 'a').replace('í', 'i').replace('é', 'e').replace('î', 'i').replace('ü', 'u').replace('è', 'e').replace('ô', 'o').replace('ú', 'u').replace('Á', 'A').replace('ä', 'a')
+    aplayerCleaned = aplayer.lower().replace(' ', '-').replace('ö', 'o').replace('ë', 'e').replace('á', 'a').replace('í', 'i').replace('é', 'e').replace('î', 'i').replace('ü', 'u').replace('è', 'e').replace('ô', 'o').replace('ú', 'u').replace('Á', 'A').replace('ä', 'a').replace('ã', 'a').replace('ñ', 'n')
     injuryurl = 'https://www.transfermarkt.com/{}/verletzungen/spieler/{}'.format(aplayerCleaned, injuryCodes[aplayer])
     response = requests.get(injuryurl, headers={'User-Agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(response.text, 'html.parser')
